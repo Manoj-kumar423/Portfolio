8f799e055ac04c7efb6bd6a2a9a060acd7181c18
Manoj-kumar423 <64767579+Manoj-kumar423@users.noreply.github.com>
2025-05-16 18:43:16 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <section id="home" style={{ padding: '50px', textAlign: 'center' }}>
       <img src="/profile.jpg" alt="Profile" width="150" style={{ borderRadius: '50%' }} />
-      <h1>Hi, I'm [Your Name]</h1>
+      <h1>Hi, I'm Manoj Kumar</h1>
       <p>DevOps Enthusiast | Cloud Learner | Explorer</p>
     </section>
   );
