5cb181b55d84e5693b97e65d128eacf82560ff86
Manoj-kumar423 <64767579+Manoj-kumar423@users.noreply.github.com>
2025-05-16 17:55:10 +0530

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section id="home" style={{ padding: '50px', textAlign: 'center' }}>
+      <img src="/profile.jpg" alt="Profile" width="150" style={{ borderRadius: '50%' }} />
+      <h1>Hi, I'm [Your Name]</h1>
+      <p>DevOps Enthusiast | Cloud Learner | Explorer</p>
+    </section>
+  );
+};
+
+export default Home;
