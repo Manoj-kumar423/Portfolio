5b3bf10a849eec2158a0f71578ceb674c00c8245
Manoj-kumar423 <64767579+Manoj-kumar423@users.noreply.github.com>
2025-05-16 17:59:01 +0530

    Create Contact.js

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section id="contact" style={{ padding: '50px', background: '#f9f9f9' }}>
+      <h2>Contact</h2>
+      <p>Email: chava.manoj99@gmail.com</p>
+      <p>GitHub: <a href="https://github.com/Manoj-kumar423">github.com/Manoj-GitHub</a></p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/manoj-kumar-chava-76a7861a5/">linkedin.com/in/Manoj-LinkedIn</a></p>
+    </section>
+  );
+};
+
+export default Contact;
